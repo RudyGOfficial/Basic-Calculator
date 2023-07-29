5e2535c7718a6ae5f46001b8b3afa5eadcc91ddc
Rudolph Gutierrez <rudygofficial@gmail.com>
2023-07-28 23:08:25 -0700

    Write a calculator to operate on simple calculations

--- 01 Basic Calculator/main.cpp
+++ 01 Basic Calculator/main.cpp
@@ -7,8 +7,62 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    double firstVal;
+    char mathOperator;
+    double secondVal;
+    int tryAgainVal;
+    bool isWrongFormat  = false;
+    bool isCalcAgain    = true;
+    
+    cout << "Calculator program. Enter a simple math calculation.\n";
+    cout << "Acceptable operators: + , - , * , /" << endl;
+    cout << "Example: 1 + 1\n\n";
+    
+    while (isCalcAgain) {
+        cout << "Enter a calculation: ";
+        cin >> firstVal >> mathOperator >> secondVal;
+            
+        switch (mathOperator) {
+            case '+':
+                cout << firstVal << " + " << secondVal << " = ";
+                cout << firstVal + secondVal << endl << endl;
+                break;
+            case '-':
+                cout << firstVal << " - " << secondVal << " = ";
+                cout << firstVal - secondVal << endl << endl;
+                break;
+            case '*':
+                cout << firstVal << " * " << secondVal << " = ";
+                cout << firstVal * secondVal << endl << endl;
+                break;
+            case '/':
+                if (secondVal != 0.0) {
+                    cout << firstVal << "/" << secondVal << " = ";
+                    cout << firstVal/secondVal << endl << endl;
+                }
+                else {
+                    cout << "Cannot divide by 0. Try again." << endl << endl;
+                }
+                break;
+            default:
+                isWrongFormat = true;
+                cout << "Wrong Format. Closing program.";
+                break;
+        }
+        
+        if (!isWrongFormat) {
+            cout << "Enter 1 to retry or any key to exit: ";
+        }
+        cin >> tryAgainVal;
+        if (tryAgainVal != 1) {
+            cout << "Closing program." << endl;
+            isCalcAgain = false;
+        }
+        cout << endl;
+    }
+
     return 0;
 }
