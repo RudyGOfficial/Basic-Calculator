b38a00e7f4fafcdbd223de85f299b6b2bfc5aec2
Rudolph Gutierrez <rudygofficial@gmail.com>
2023-07-28 22:53:01 -0700

    Initial Commit

---
+++ 01 Basic Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  01 Basic Calculator
+//
+//  Created by Rudolph Gutierrez on 7/28/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
